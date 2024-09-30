e769f7023fbbe92e13b2c49601a0fc83ac2bd648
isuhi <154265353+isweee@users.noreply.github.com>
2024-09-30 12:27:29 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,47 @@
+Last login: Mon Sep 30 12:20:25 on ttys000
+isuhi@Isuhis-Laptop ~ % python3 -m idlelib.idle
+** IDLE can't import Tkinter.
+Your Python may not be configured for Tk. **
+isuhi@Isuhis-Laptop ~ % cd CS104
+isuhi@Isuhis-Laptop CS104 % python
+zsh: command not found: python
+isuhi@Isuhis-Laptop CS104 % clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+isuhi@Isuhis-Laptop CS104 % python3
+Python 3.12.6 (main, Sep  6 2024, 19:03:47) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> # This is Version 1 of my hello world program
+>>> print("Hello my name is Isuhi")
+Hello my name is Isuhi
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
